ccb5027a433e9ee0f256c29e63310f3a7ae0d3f9
xkyl-yhw <xkylhh@163.com>
2022-03-12 20:23:20 +0800

    2022-3-12-20：23 yhw

--- Assets/Scripts/UI/CameraDisplay.cs
+++ Assets/Scripts/UI/CameraDisplay.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 [System.Serializable]
 public struct CameraPropert {
-    GameObject _camera_obj;
-    Camera _camera;
-    string name;
+    public GameObject _camera_obj;
+    public Camera _camera;
+    public string name;
 }
 
 public class CameraDisplay : MonoBehaviour
@@ -14,4 +16,31 @@ public class CameraDisplay : MonoBehaviour
     public int cameraNum = 0;
     [SerializeField]
     public List<CameraPropert> cameraList = new List<CameraPropert>();
+    public GameObject ImagePrefab;
+    public Transform ImageTran;
+    public List<RawImage> imageList = new List<RawImage>();
+
+    private void Start()
+    {
+        for (int i = 0; i < cameraList.Count; i++)
+        {
+            CameraPropert temp = cameraList[i];
+            temp._camera = cameraList[i]._camera_obj.GetComponent<Camera>();
+            cameraList[i] = temp;
+            GameObject tempImage = GameObject.Instantiate(ImagePrefab, ImageTran);
+            tempImage.GetComponent<Button>().onClick.AddListener(()=> { this.GetComponent<CameraDisplay>().turnView(); });
+            imageList.Add(tempImage.GetComponent<RawImage>());
+
+            RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 0);
+            cameraList[i]._camera.targetTexture = rt;
+            imageList[i].texture = rt;
+        }
+    }
+
+    public void turnView()
+    {
+        int index = EventSystem.current.currentSelectedGameObject.transform.GetSiblingIndex();
+        Camera.main.transform.position = cameraList[index]._camera_obj.transform.position;
+        Camera.main.transform.rotation = cameraList[index]._camera_obj.transform.rotation;
+    }
 }
