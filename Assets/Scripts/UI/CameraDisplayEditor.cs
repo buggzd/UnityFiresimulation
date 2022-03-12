5b530f6a71da254e88ae0ca89b39cd74bf1057d4
xkyl-yhw <xkylhh@163.com>
2022-03-12 17:51:44 +0800

    2020-3-12-17：51 yhw

---
+++ Assets/Scripts/UI/CameraDisplayEditor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraDisplay))]
+public class CameraDisplayEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("update"))
+        {
+
+        }
+    }
+}
