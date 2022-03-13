834bf42b65aeead787e0bcfe4965d3202cd9b0c9
xkyl-yhw <xkylhh@163.com>
2022-03-13 15:20:30 +0800

    2022-3-13-15:20 yhw

---
+++ Assets/Scripts/UI/AlarmSignUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.EventSystems;
+
+public class AlarmSignUI : MonoBehaviour
+{
+    public Dictionary<GameObject, GameObject> alarmList = new Dictionary<GameObject, GameObject>();
+    public GameObject tempAlarmSign;
+    public Transform alarmTran;
+    public Vector2 _offset;
+
+
+    private void Start()
+    {
+        foreach (var item in GameObject.FindGameObjectsWithTag("alarm"))
+        {
+            GameObject tempSign = Instantiate(tempAlarmSign, alarmTran);
+            alarmList[item] = tempSign;
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var item in alarmList)
+        {
+            if (InView(item.Key.transform.position)){
+                Vector2 vec2 = Camera.main.WorldToScreenPoint(item.Key.transform.position);
+                item.Value.SetActive(true);
+                item.Value.transform.position = vec2 + _offset;
+            }
+            else
+            {
+                item.Value.SetActive(false);
+            }
+        }
+    }
+
+    bool InView(Vector3 pos)
+    {
+        Transform camTran = Camera.main.transform;
+        Vector2 viewPos = Camera.main.WorldToViewportPoint(pos);
+        Vector3 dir = (pos - camTran.position).normalized;
+        float dot = Vector3.Dot(camTran.forward, dir);
+        if (dot > 0 && viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1)
+            return true;
+        else return false;
+    }
+
+}
